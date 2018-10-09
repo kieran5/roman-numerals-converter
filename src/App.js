054a92934c310b7de36dc262e2a6f14de8a55acb
kieran5 <kieranwillisx@gmail.com>
2018-10-09 16:53:37 +0100

    Improved layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    margin: theme.spacing.unit,
+  },
+  root: {
+    width: '80%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+    marginLeft: '10%',
+    marginRight: '10%',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -21,12 +51,14 @@ class App extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     const listConversions = this.state.conversions.map((conversion) =>
-      <tr key={conversion._id}>
-        <td>{conversion.date}</td>
-        <td>{conversion.from}</td>
-        <td>{conversion.to}</td>
-      </tr>
+      <TableRow key={conversion._id}>
+        <TableCell>{conversion.date}</TableCell>
+        <TableCell>{conversion.from}</TableCell>
+        <TableCell>{conversion.to}</TableCell>
+      </TableRow>
     );
     return (
       <div className="App">
@@ -36,23 +68,33 @@ class App extends Component {
 
         <div>
           Converter
+          <div>
+            <Input
+              placeholder="Enter value to convert"
+            />
+            <Button variant="contained" color="primary" className={classes.button}>Convert</Button>
+          </div>
         </div>
+
+        <hr/>
 
         <div>
           Recent Conversions
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>From</th>
-                <th>To</th>
-              </tr>
-            </thead>
+          <Paper className={classes.root}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>From</TableCell>
+                  <TableCell>To</TableCell>
+                </TableRow>
+              </TableHead>
 
-            <tbody>
-              {listConversions}
-            </tbody>
-          </table>
+              <TableBody>
+                {listConversions}
+              </TableBody>
+            </Table>
+          </Paper>
         </div>
 
       </div>
@@ -60,4 +102,8 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
